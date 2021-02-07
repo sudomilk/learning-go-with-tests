f4c639c4c55c71a301985b3121a32c545ef0d068
sudomilk <mailertaylor@gmail.com>
2021-02-07 09:58:01 -0600

    added no error test

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -9,13 +9,16 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -32,7 +35,14 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	got := wallet.Balance()
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
@@ -43,6 +53,6 @@ func assertError(t testing.TB, got error, want error) {
 	}
 
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
